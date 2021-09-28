df8946c150b96220074a833e5ac107110b2777da
E-sa <35602280+E-sa@users.noreply.github.com>
2021-09-28 14:08:22 +0330

    div changed to span

--- src/components/LastLiSmallScreen.js
+++ src/components/LastLiSmallScreen.js
@@ -3,7 +3,7 @@ import react from "react";
 function LastLiSmallScreen({ filter, changeFilter }) {
 
     return (
-        <div id="filters-span-s">
+        <span id="filters-span-s">
             <li className="each-task" id="last-li-s">
 
                 <span
@@ -30,7 +30,7 @@ function LastLiSmallScreen({ filter, changeFilter }) {
                     Completed
                 </span>
             </li>
-        </div>
+        </span>
     )
 
 }
